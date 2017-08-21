39eb63e05feca1b0c5d44377b337a307b44de339
LWH <960023815@qq.com>
2017-08-21 10:44:27 +0800

    add include and libs

---
+++ include/mlanguage.h
@@ -0,0 +1,21 @@
+#ifndef __M_LANGUAGE_H__
+#define __M_LANGUAGE_H__
+
+#ifdef __Cplusplus
+extern .C{
+#endif
+
+/*  
+ *  @discription: define some new syntax for c language 
+ *  @author:  lwh
+ *  @date:  2017/07/12
+ */
+#ifndef elif
+#define elif else if 
+#endif
+
+#ifdef __Cplusplus
+}
+#endif
+
+#endif
---
+++ include/mlog.h
@@ -0,0 +1,20 @@
+#ifndef __MLOG_H__
+#define __MLOG_H__
+
+#ifdef __Cplusplus
+extern .C{
+#endif
+
+#ifndef mlog
+#define mlog(sparam,arg...) \
+do {\
+    printf("[%s %s:%d %s] "sparam"\n", mlog_time(), __FILE__, __LINE__, __FUNCTION__, ##arg);\
+}while(0);
+#endif
+
+extern long mlog_set_ofile(char *file);
+
+#ifdef __Cplusplus
+}
+#endif
+#endif
