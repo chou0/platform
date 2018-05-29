3bd430233be3297bfebe629d50847fdd4684b5c6
LWH <960023815@qq.com>
2018-05-29 19:52:42 +0800

    add net send and recv func

--- src/mnet.c
+++ src/mnet.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <errno.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <fcntl.h> 
+
+extern int errno;
 
 int mnet_setnoblock(int fd)
 {
@@ -59,3 +62,64 @@ fail_label:
     close(client);	
 	return ret;
 }
+
+int mnet_send(int fd, unsigned char *buf, unsigned int len)
+{
+    int ret;
+    unsigned char *sendptr;
+
+    sendptr = buf;
+    while (len > 0)
+    {
+        ret = send(fd, sendptr, len , 0);
+        if (ret < 0)
+        {
+            if (errno == EAGAIN)
+            {
+                continue;
+            }
+            else if (errno == EPIPE)
+            {
+                return -1;
+            }
+        }
+        
+        len -= ret;
+        sendptr += ret;
+    }
+
+    return 0;
+}
+
+int mnet_recv(int fd, unsigned char *buf, unsigned int len)
+{
+    int ret;
+    unsigned char *readptr;
+
+    readptr = buf;
+    while (len > 0)
+    {
+        ret = recv(fd, readptr, len , 0);
+        if (ret < 0)
+        {
+            if (errno == EAGAIN)
+            {
+                continue;
+            }
+            else if (errno == EPIPE)
+            {
+                return -1;
+            }
+        }
+        else if (ret == 0)
+        {
+            return -1;
+        }
+        
+        len -= ret;
+        readptr += ret;
+    }
+
+    return 0;
+}
+
